b29fc193d8b7044bf3eb0d4593ca7ca9e407e415
Clouie-png <Clouie_lazatin@rcc.edu.ph>
2024-09-28 10:08:43 +0800

    first commit

---
+++ src/StringOutput.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const StringOutput = () => {
+
+    return (
+    <div>
+        <p>1.  	Create a reactjs project that outputs a string value(20pts)</p>
+    </div>
+  )
+}
